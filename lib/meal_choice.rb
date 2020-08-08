b95f8a12a964e9bc93dd4fa3d2ffea80321ad81a
David Molina <davidmolina2810@gmail.com>
2020-08-08 18:54:28 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,7 +4,7 @@
 #method that will accept 2 vegetables and, optionally, a protein (default='tofu') and
 #out put two strings, returning One
 def meal_choice(veg1, veg2, protein='tofu')
-  puts "What a nutricious meal!"
+  puts "What a nutritious meal!"
   phrase = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts phrase
   phrase
